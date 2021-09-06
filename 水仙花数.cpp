49280b5f6c7b0b3d56bc9529593c80aa8df70b25
kaibiao <884484242@qq.com>
2021-09-06 22:21:08 +0800

    c++写水仙花数

---
+++ 水仙花数.cpp
@@ -0,0 +1,20 @@
+#include "iostream"
+using namespace std;
+int main(){
+  int num,a,b,c;
+  for(num=100;num<1000;num++){
+    
+    
+    a = num/100;
+    b = num%10;
+    c = num/10%10;
+
+    if(a*a*a+b*b*b+c*c*c==num){
+      cout << num << endl;
+    }
+  }
+  
+
+  system("pause");
+  return 0;
+}
